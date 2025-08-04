ed7d48496cb3f1b874e54a8b5a8f7e921eef6604
Graham Norbury <gnorbury@bondcar.com>
2025-08-03 20:22:01 -0400

    Basic install script

---
+++ install.sh
@@ -0,0 +1,34 @@
+#! /bin/sh
+
+# gr-osmosdr install script for debian based systems
+
+GR_VER=$(apt list gnuradio 2>/dev/null | grep -m 1 gnuradio | cut -d' ' -f2 | cut -d'.' -f1,2)
+echo "Identified GNURadio version ${GR_VER}"
+if [ ${GR_VER} = "3.10" ]; then
+    echo "Installing for GNURadio 3.10"
+else
+    echo "Installing for GNURadio ${GR_VER} is not supported by this version of op25"
+    echo "Please use git branch \"gr38\" for GNURadio-3.8 or earlier"
+    exit 1
+fi
+
+GR_OSMOSDR=$(apt list gr-osmosdr 2>/dev/null | grep -o '\[.**\]')
+echo "Checking for gr-osmosdr package ${GR_OSMOSDR}"
+if [ "${GR_OSMOSDR}" != "" ]; then
+    echo "The gr-osmosdr package is presently installed. Please remove it before"
+    echo "re-running this install script."
+    exit 1
+fi
+
+rm -rf build
+mkdir build
+cd build
+echo "Configuring..."
+cmake ../         2>&1 | tee cmake.log
+echo "Compiling..."
+make              2>&1 | tee make.log
+echo "Installing..."
+sudo make install 2>&1 | tee install.log
+sudo ldconfig
+echo "...installation complete"
+
